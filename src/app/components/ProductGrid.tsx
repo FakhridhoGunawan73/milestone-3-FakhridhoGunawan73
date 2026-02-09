6d0db65d27a78b3ae8e7dd94ef0a3d9040dcb37b
FakhridhoGunawan73 <fakhridhogunawan01@gmail.com>
2026-02-10 01:57:10 +0700

    feat: improve layout responsiveness and add dummy data fallback

--- src/app/components/ProductGrid.tsx
+++ src/app/components/ProductGrid.tsx
@@ -7,7 +7,7 @@ interface ProductGridProps {
 export default function ProductGrid({ children }: ProductGridProps) {
   return (
     <main className="p-10 mx-w-7xl px-6">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">{children}</div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">{children}</div>
     </main>
   );
 }
--- src/app/components/ProductHeader.tsx
+++ src/app/components/ProductHeader.tsx
@@ -2,7 +2,7 @@ import { ProductHeaderProps } from "../types/product";
 
 function ProductHeader({ count }: ProductHeaderProps) {
   return (
-    <main className="max-w-8xl mx-auto mt-6 px-10">
+    <main className="max-w-5xl mx-auto py-8 bg-slate-50">
       <div className="text-2xl font-bold">Products</div>
       <p>Showing {count} items</p>
     </main>
